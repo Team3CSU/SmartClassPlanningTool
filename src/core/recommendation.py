e05682d60cf0c17bd872078a26e47dfd791da490
Akshith <akshithnukala9@gmail.com>
2023-10-22 12:29:24 -0400

    Generate course schedule phase 1

--- src/core/recommendation.py
+++ src/core/recommendation.py
@@ -13,22 +13,131 @@ def get_prerequisite(course: str, graph: DiGraph):
             return edge[1]
 
 
+def get_prerequisites(course: str, graph: DiGraph):
+    courses = []
+    for edge in graph.edges():
+        if edge[0] == course:
+            courses.append(edge[1])
+    return courses
+        
+def cleanCourseCode(coursesRaw:list):
+    courses = []
+    for course in coursesRaw:
+        course = course if "*" not in course else course[:course.find("*")]
+        course = course.strip()
+        course = " ".join(course.split());
+        courses.append(course)
+    return courses
+
+def newAcademicYr(): 
+    semester_dict= {"Fa":list().copy(),"Sp":list().copy(),"Su":list().copy()}
+    return semester_dict
+    
+
+def sorter(deps):
+    if(not deps): return -1;
+    depth = 0
+    print(deps)
+    for code,crsdepth in deps:
+        depth =max(depth,crsdepth)
+    return depth
+
+def extractPreq(courses_to_take,course, graph,lvl=0):
+    dependencies = []
+    print(f"\n\n\nchecking prerequesites for {course}")
+    if has_prerequisites(course, graph):
+        preqCourses = get_prerequisites(course, graph)
+        print(preqCourses)
+        for preq in preqCourses:
+            if preq is not None:
+                if preq not in courses_to_take:
+                    courses_to_take.append(preq)
+                dependencies.append((preq,lvl))
+                print(f"dependenices Now",dependencies)
+                if has_prerequisites(preq, graph):
+                    dep_in_depth,crsestake_indpth = extractPreq(courses_to_take,preq, graph,lvl+1)
+                    dependencies.extend(dep_in_depth)
+                    courses_to_take = crsestake_indpth
+    return dependencies,courses_to_take
 def generate_degree_plan(text: dict, graph: DiGraph, Courseshedule:dict=None):
     courses_to_take = []
 
+    courses_plan_dict = {}
     # run a loop over all the keys of courses that have to be taken
     for key in text.keys():
         # extract courses for each key
         coursesRaw = text[key]
         counter = 0
         limit = 0
-        courses = []
-        for course in coursesRaw:
-            course = course if "*" not in course else course[:course.find("*")]
-            course = course.strip()
-            course = " ".join(course.split());
-            courses.append(course)
+        courses = cleanCourseCode(coursesRaw)
+        for course in courses:
+            dependencies = []
+            if "SELECT".lower() in course.lower():
+                # if there is a selection criteria, set it as a limit
+                limit = int(course.split(" ")[1].strip())
+            if limit > 0 and counter == limit:
+                # if the selection criteria has been satisfied break the inner loop
+                break
+            if "SELECT".lower() not in course.lower():
+                original_course = course
+                print(f"||{course}||is having pre requesite" if has_prerequisites(course, graph) else f"||{course}|| no preq")
+                dependencies,courses_to_take = extractPreq(courses_to_take,original_course, graph)
+                print(dependencies)
+                courses_plan_dict[original_course] = dependencies
+                if original_course not in courses_to_take: courses_to_take.append(original_course)
+                counter = counter + 1
+    # return courses_to_take,courses_plan_dict
+    Sems = {1:newAcademicYr(),2:newAcademicYr()}
+
+
+    for course in sorted(courses_plan_dict,key=lambda x:sorter(courses_plan_dict[x]),reverse=True):
+        print(f'{course} dependeinces:\t\t\t:',end=" ")
+        for dep in courses_plan_dict[course]:
+            print(f'\t\t{dep}',end="")
+        print()
+    
+    print("stating acad planner",f"for {len(courses_to_take)}, {len(set(courses_to_take))}")
+    print(*courses_to_take,sep="\t")
+    coursesTaken=AcadPlanner(courses_plan_dict,Courseshedule,graph,courses_to_take,Sems)
+    for crs in courses_to_take:
+        if crs not in coursesTaken:
+            print(crs,"Not taken why why")
+
+    print("stating acad planner",f"for {len(courses_to_take)}, {len(set(courses_to_take))}")
+    print(*courses_to_take,sep="\t")
+    print("stating acad planner",f"for {len(coursesTaken)}, {len(set(coursesTaken))}")
+    print(*coursesTaken,sep="\t")
+
+def AcadPlanner(courses_plan_dict,Courseshedule,graph,Sems,courses_to_take,coursesTaken=None):
+    if(not coursesTaken): coursesTaken=list()
+    for course in sorted(courses_plan_dict,key=lambda x:sorter(courses_plan_dict[x]),reverse=True):
+        dependencies = courses_plan_dict.get(course, extractPreq([course],course,graph)[0])
+        # if no dep, place it
+        if(len(dependencies)):
+            # iterate dependecies depth wise
+            for dep in sorted(dependencies,key=lambda x : x[1],reverse=True):
+                print(f"planning {course} with {dep}")
+                AcadPlanner({dep[0]:extractPreq([dep[0]],dep[0],graph)[0]},Courseshedule,graph,Sems,coursesTaken)
         
+        if course in coursesTaken:
+            print(f"Already placed")
+            continue
+        coursesTaken.append(course)
+        print(f"{course} is being placed")
+        
+    return coursesTaken
+    
+
+
+def generate_degree_plan_phase1(text: dict, graph: DiGraph, Courseshedule:dict=None):
+    courses_to_take = []
+    # run a loop over all the keys of courses that have to be taken
+    for key in text.keys():
+        # extract courses for each key
+        coursesRaw = text[key]
+        counter = 0
+        limit = 0
+        courses = cleanCourseCode(coursesRaw)
         for course in courses:
             if "SELECT".lower() in course.lower():
                 # if there is a selection criteria, set it as a limit
